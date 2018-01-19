eb99b5567d79b912c9cbe6deef0a10a1d6308382
Ginger Lee Kretschmer <kretschmons@gmail.com>
2018-01-19 09:41:17 -0800

    add animals component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  title = 'Angular Ape App';
+  // masterAnimalList = Animal[] = [
+  //   new Animal()
+  // ]
 }
