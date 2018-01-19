ea823c7547050aad8caa49e0d30613ddf0a7e23f
Ginger Lee Kretschmer <kretschmons@gmail.com>
2018-01-19 15:43:48 -0800

    add select form for filter pipe

--- src/app/animals-list/animals-list.component.ts
+++ src/app/animals-list/animals-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnChange, Input, Output, EventEmitter } from '@angular/core';
 import { Animal } from '../animal';
 import { FilterPipe } from '../filter.pipe';
 
@@ -6,19 +6,21 @@ import { FilterPipe } from '../filter.pipe';
   selector: 'app-animals-list',
   templateUrl: './animals-list.component.html',
   styleUrls: ['./animals-list.component.scss'],
-  pipes: [FilterPipe]
 })
+
 export class AnimalsListComponent implements OnInit {
+
   @Input() childAnimalList: Animal[];
   @Output() clickSender = new EventEmitter();
 
   constructor() { }
 
-  selectAnimalButtonClicked(animal: Animal) {
-    this.clickSender.emit(animal);
+  editAnimalButtonClicked(animalToEdit: Animal) {
+    this.clickSender.emit(animalToEdit);
   }
 
-  ngOnInit() {
-  }
+  onChange(optionFromMenu) {
+  this.filterByAge = optionFromMenu;
+}
 
 }
--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -2,15 +2,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { Animal } from './animal';
 
 @Pipe({
-  name: 'filter'
+  name: 'age',
+  pure: false
 })
 
 export class FilterPipe implements PipeTransform {
 
-  transform(animals: Animal[], age: any) {
+  transform(animals: Animal[], ageSelect: any) {
     let output: Animal[] = [];
 
-    if (age === 'young') {
+    if (ageSelect === 'young') {
       for (let i = 0; i < animals.length; i++) {
         if (animals[i].age < 2) {
           output.push(animals[i]);
@@ -18,7 +19,7 @@ export class FilterPipe implements PipeTransform {
         }
         return output;
 
-    } else if (age === 'mature') {
+    } else if (ageSelect === 'mature') {
       for (let i = 0; i < animals.length; i++) {
         if (animals[i].age >= 2) {
           output.push(animals[i]);
@@ -27,7 +28,7 @@ export class FilterPipe implements PipeTransform {
         return output;
       } else {
         return animals;
-      }
-
+    }
   }
+
 }
