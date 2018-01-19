0a6e2ec27e78f8007146e0c916d178506ad48aba
Ginger Lee Kretschmer <kretschmons@gmail.com>
2018-01-19 14:02:34 -0800

    add in more animal objects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,18 @@ import { Animal } from './animal';
 export class AppComponent {
   title = 'Angular Ape App';
   masterAnimalList: Animal[] = [
-    new Animal('Artic Fox', 'Sir Fox-a-lot', 3, 'field mice', 'Zoo Snow Fields', 3, 'Male', 'Pouncing', 'Heat'),
-    new Animal('Hawaiian Monk Seal', 'Sealy', 10, 'Kelp', 'Zoo Ocean', 1, 'Female', 'Surfing', 'Sharks')
+    new Animal('Artic Fox', 'Sir Fox-a-lot', 1, 'field mice', 'Zoo Snow Fields', 3, 'Male', 'Pouncing', 'Heat'),
+    new Animal('Hawaiian Monk Seal', 'Sealy', 10, 'Kelp', 'Zoo Ocean', 1, 'Female', 'Surfing', 'Sharks'),
+    new Animal('Giant Panda', 'Pandala', 6, 'Bamboo', 'Zoo Forest', 2, 'Female', 'Eating', 'Hunger')
   ];
   selectedAnimal = null;
 
   selectAnimal(clickedAnimal) {
     this.selectedAnimal = clickedAnimal;
+  }
+
+  done() {
+  this.selectedAnimal = null;
   }
 
   addAnimal(newAnimalFromChild: Animal){
